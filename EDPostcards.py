2b64a5a09cfe16b66e25a9385ad6a5cad86456dc
Foxlider <al.loncham@gmail.com>
2017-04-20 23:50:28 +0200

    Update 1.5a

--- EDPostcards.py
+++ EDPostcards.py
@@ -10,7 +10,6 @@
 # Installation : 
 #   Just get the keys from twitter (CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET) 
 #   Put them in a file called keyFile.txt in this EXACT order ! 
-#   Create a 'dl' folder
 #   Launch the BOT
 #   TADAAAAA !
 #
@@ -22,27 +21,37 @@
 
 # THE END
 
-
 #Basic informations 
-__program__ = "EDP Bot"
-__version__ = "1.3a"
+__program__ = "EDPostcards Bot"
+__version__ = "1.5a"
 
 ##Libraries imports
 import datetime
-import tweepy
-import wget
-import time
 import sys
+try:
+    assert sys.version_info >= (3, 4)
+    import tweepy
+    import wget
+    import json
+except ImportError:
+    logError(100,__program__+"needs tweepy, wget and json to work properly. Please install them using pip install <module>. ")
+    sys.exit()
+except AssertionError:
+    logError(101, __program__+'needs python 3.4 or greater to work properly. Please see the installation guide for your Operating System.')
+    sys.exit()
+import time
+import random
 import os
 
-
-
+#TODO:   every text in logs
 
 ##Variables setup
-keysFile=open("keyFile.txt","r")
+try:
+    keysFile=open("keyFile.txt","r")
+except Exception as error:
+    print(str(error) + "\nkeyFile doesn't exists. What have you done ? Nooooo ! *couic*")
+    quit();
 keys=keysFile.readlines()
-consumer_key=keys[0].rstrip() # .rstrip() gets us rid off of \n
-consumer_secret=keys[1]
 keysFile.close()
 
 CONSUMER_KEY = keys[0].rstrip()
@@ -53,29 +62,128 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 media_files = set()
+bigBoss = ["Foxlidev","FoxliderAtom"] #Put here your Twitter name to be an admin
+#Add texts here to increase the BOT's vocabulary and allow it to say more things ! 
+quoteText = dict(quote1='They told me to quote this and I  refused. But those pics are great, so...', 
+quote2='OMG I love those ! This guy is a genius !',
+quote3='My program is telling me that this stuff is awesome and that I must quote it.',
+quote4='Oh my ! You really should watch this ! I am clearly not overreacting ! ',
+quote5="This images are splendid. I'm happy to share them with you.",
+quote6="I may be a robot, but I know how to appreciate real ART !",
+quote7="I must... Resist... To... QUOTE ! NOOOOOOO ! ",
+quote8='You want some pics ? You love those pics... Take those pics ! ',
+quote9='His work is awesome. Check this out ! ',
+quote10='Wow ! I liked this game but now i really love it ! ',
+quote11='Is that some Elite Dangerous screenshots ? ',
+quote12="You know you want these screenshots. You want them ! ",
+quote13="\"From space, with love\" -ED_Postcards",
+quote14="Fly safe, CMDRs !",
+quote15="These are incredible ! Check this out !",
+quote16="I don't know what to say. It's just wonderful ! ",
+quote17="This is it. I am in love.",
+quote18="I have one word only in mind : Incredible",
+quote19="I have one word only in mind : Splendid",
+quote20="This game offers some pretty postcards for sure !",
+quote50="I am running out of ideas for texts. So... Here, take this.")
+
+#The filter
+hashtag = "EDPostcards"
 
 global datadir
 datadir = os.path.dirname("./data/")
 if not os.path.exists(datadir):
     os.makedirs(datadir)
-
+if not os.path.exists("./dl/"):
+    os.makedirs("./dl")
 
 ##Functions and classes
 
-def dl(media_files):
+def dl(media_files,sender):
+    """
+        This function is used to download the status medias if there is any. 
+        params : 
+            media_files : list of the urls of each medias from the status
+            sender      : screen_name of the status sender
+        return a list of the files urls
+    """
     i = 0
+    folder = "dl/"+sender+"/"
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    today = datetime.datetime.now()
+    
     dl_files = set()
     for url in media_files:
         i+=1
-        print("downloading media "+str(i)+" : " +url)
+        name = str(today.day) + "-" + str(today.month) + "_" + str(today.hour) + "h" + str(today.minute) + "_" + str(i) + '.jpg'
+        logText("downloading media "+str(i)+" : " +url + "to " + folder + name)
         try:
-            wget.download(url, out = "dl\img"+str(i)+".jpg")
+            wget.download(url, out = folder+name)
+        except Exception as error:
+            logText(str(error)+"\nUnable to download file. Cancelling.")
+            logError(156, str(error)+"\nUnable to download file.")
         except:
-            print('Unable to download file. Cancelling.')
-        dl_files.add("dl\img"+str(i)+".jpg")
+            logText("Oops I fuckedup... You didn't wanted this anyway.")
+            logError(157, "Nomething bad happened...")
+        dl_files.add(folder+name)
+    media_files = set()
     return dl_files
+   
+def fshutdown():
+    """
+        This function is supposed to shut down the bot by disconnecting each stream.
+    """
+    logText("Logging off...")
+    shutdown = True
+    loop = False
+    api.update_profile(location="In a debris field")
+    mainStream.disconnect()
+    cmdStream.disconnect()
+    logText("Shuting down...")
+        
+def frestart():
+    """
+        this function is supposed to shutdown then restart the bot
+    """
+    fshutdown()
+    python = sys.executable
+    #FIXME: Need to restart the program
+    #os.execl(python, python, * sys.argv)
 
+def cmdHandler(cmd, orig=True):
+    """
+        The cmdHandler is here to handle commands to control the BOT
+        params:
+            cmd     : command in text
+            orig    : True if it comes from the console, False if it comes from direct messages
+        calls another cmdHandler if the command is not restart or shutdown
+    """
+    if (cmd == "shutdown" or cmd == "Shutdown"): #TODO: Improvements for shutdown
+        fshutdown()
+        if not orig:
+            logText("Program shutdown command called.")
+            sys.exit(0) #BUG: Don't exit but dunno why
+            python = sys.executable #FIXME: Using this instead
+    elif cmd == "restart" or cmd =="Restart": #TODO: Improvements for restart
+        frestart()
+    else:
+        if (cmd.startswith("tweet ")):
+            text = cmd[6:]
+            try:
+                api.update_status(status=text)
+            except tweepy.TweepError as error:
+                logError(error.args[0][0]['code'], error.args[0][0]['message'])
+        if (cmd.startswith("foo ")):
+            text = cmd[4:]
+            logText(text)
+        
+        if orig: #If the command was sent through the console, display the message again
+            cmdHandler(input("Command ? : \n>>>")) 
+            
 def logStart():
+    """
+        Function used to log in a file at startup
+    """
     dir = os.path.dirname("./logs/")
     if not os.path.exists(dir):
         os.makedirs(dir)
@@ -84,48 +192,116 @@ def logStart():
     log="[" + str(today) + "] : Bot " + __program__ + " v" + __version__ + " logged in\n"
     logsFile.write(log)
     print(log,end='')
-    
-def logStatus(status,media):
-    dir = os.path.dirname("./logs/")
+
+def logText(text):
+    """
+        Function used to log text in a file and printing it in the console.
+        Replacing usual print()
+        params : 
+            text    : text to print
+    """
+    dir = os.path.dirname("./logs/console/")
     if not os.path.exists(dir):
         os.makedirs(dir)
     today = datetime.datetime.now()
-    logsFile=open("./logs/" + str(today.day) + "-" + str(today.month) + "-" + str(today.year) + ".txt","a")
-    log = str(today.hour) + ":" + str(today.minute) + ":" + str(today.second) + " [" +  str(status.author.screen_name) + "] : " + status.text + "\n"
+    logsFile=open(dir +"/"+ str(today.year) + "-" + str(today.month) + "-" + str(today.day) + ".txt","a")
+    log = "[" + str(today.hour) + ":" + str(today.minute) + ":" + str(today.second) + "] " + text + "\n"
     logsFile.write(log)
-    if (media):
-        logsFile.write("Media detexted\n")
-    else:
-        logsFile.write("No media detected")
-    print(log,end='')
+    print(log, end='')
+    logsFile.close()
 
-class MyStreamListener(tweepy.StreamListener):
-    def on_status(self, status):
-        author = status.author.screen_name
-        text = status.text
-        if (not text.startswith("RT")):
-            print("@"+author + " : \n"+text)
-            #print(vars(status))
-            media = status.entities.get('media', []) 
-            if(len(media) > 0 ):
-                print (str(len(media))+" media(s) detected")
-                logStatus(status,True)
-                media_files.add(media[0]['media_url']) #IMPROVE IT !
-                dl_files = dl(media_files)
-                id = status.id_str
-                api.retweet(id) # I can't seems to get more than 1 media per tweet and can't seems to send more than 1 media per tweet. So better RT
-            else:
-                print("No media detected.\n")
-                logStatus(status,False)
-    
-    def on_direct_message(self, status): #Can't handle direct message yet. Why ? Dunno.
-        author = status.author.screen_name 
-        text = status.text
-        print(author + " : \n"+text)
-        logStatus(status,False)
+def logError(errnum = 0, errtext=""):
+    """
+        Function used to print errors in a file
+        params: 
+            errnum      : code of the error between 100 and 999
+            errtext     : details of the error
+    """
+    dir = os.path.dirname("./logs/errorLogs/")
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    today = datetime.datetime.now()
+    logsFile=open(dir +"/"+ str(today.year) + "-" + str(today.month) + "-" + str(today.day) + ".txt","a")
+    log = "[" + str(today.hour) + ":" + str(today.minute) + ":" + str(today.second) + "] 0x0" + str(errnum) + " : " + errtext + "\n"
+    logsFile.write(log)
+    print(log, end='')
+    logsFile.close()
+
+def statusTreatment(decoded):
+    """
+        specific status treatment function.
+        callend to handle the hashtag when needed
+        params : 
+            decoded     : The status
+    """
+    id = decoded["id"]                                                  #IMPORTANT DATA VARS
+    text = decoded["text"]                                              #|
+    sender = decoded["user"]["name"]                                    #|
+    sendertag = decoded["user"]["screen_name"]                          #|
+    #○dump(decoded)
+    logText(hashtag + " in status text")                               #Write to logs
+    media = set()
+    media_files = set()
+    if ("extended_entities" in decoded):                                #if medias were sent
+        media = decoded["extended_entities"]["media"]                   #go get 'em    
+    logText("Status n°"+str(id)+" by @"+ sender + "\n\""+text+"\" ("+str(len(media)) + " media files)")
+    if (len(media)>=1):                                                 #Did we got some medias ?
+        for i in media:
+            media_files.add(i["media_url_https"])                           #Add medias to var
+        dl(media_files,sender)                                          #Get those medias in your files
+        qcode, qtext = random.choice(list(quoteText.items()))           #Get one of the quotenswers
+        api.update_status(status = qtext+" https://twitter.com/"+sendertag+"/status/"+str(id))
+
+def statusHandler(decoded):
+    """
+        main status treatment function.
+        called to handle every status received and calling statusTreatment if needed
+        params : 
+            decoded     : The status
+    """
+    id = decoded["id"]                                                  #IMPORTANT DATA VARS
+    text = decoded["text"]                                              #|
+    sender = decoded["user"]["name"]                                    #|
+    sendertag = decoded["user"]["screen_name"]                          #|
+    if (sendertag == me.screen_name):                                   #IF BOT sending tweets to itself
+        #api.send_direct_message(screen_name=sendertag, text="Sending tweets yourself again ?")
+        logText("Receiving tweet from myself : " + text)
+    elif ("#" + hashtag in text and not text.startswith("RT ") ):      #MAIN : hashtag handling / not an RT
+        statusTreatment(decoded)
+        
+def directMessageHandler(decoded):
+    """
+        direct messages treatment function.
+        called to handle every direct message received
+        params : 
+            decoded     : The status
+    """
+    id = decoded["direct_message"]["id"]                            #IMPORTANT DATA VARS
+    text = decoded["direct_message"]["text"]                        #|
+    sender = decoded["direct_message"]["sender"]["name"]            #|
+    sendertag = decoded["direct_message"]["sender"]["screen_name"]  #|
+    logText("Direct Message n°"+str(id)+"\n\""+text+"\"")           #Write to logs
+    if sender in bigBoss and sendertag != me.screen_name:           #IF PV sent by me and not by the BOT (avoid repeating commands)
+        logText ("Receiving direct message from admin : "+text)     #Write to logs
+        api.send_direct_message(screen_name=sendertag, text="Handling "+text+" command... Please wait...")                                                 #Send message
+        cmdHandler(text, False)                                     #cmdHandler will se if a command is sent or not
+
+class mainStreamListener(tweepy.StreamListener): #MAIN STREAM TO HANDLE HASHTAG TREATMENT
+    #TODO: Make functions to handle treatments
+    def on_data(self, status):
+        dump(self)
+        decoded = json.loads(status)                                #Decode from json
+        print("====================")                               #Separator          
+        #dump(decoded)                                              #UNCOMMENT THIS TO DUMP DATA
+        if("direct_message" in decoded):                        #======= DATA IS DIRECT MESSAGE
+            directMessageHandler(decoded)
+        elif("in_reply_to_status_id" in decoded and not "event" in decoded): #======= DATA IS STATUS
+            statusHandler(decoded)
+        #TODO: Improve tweets
         
     def on_error(self, status_code):
-        print ("Error Code: " + str(status_code))
+        logText ("Error Code: " + str(status_code))
+        logError(status_code, "Twitter API error. Refer to dev.twitter.com for more informations")
         if status_code == 420:
             return False
         else:
@@ -134,6 +310,56 @@ class MyStreamListener(tweepy.StreamListener):
     def on_timeout(self):
         print('Timeout...')
         return True
+        
+class cmdStreamListener(tweepy.StreamListener): #THIS ONE IS USED FOR COMMANDS HANDLING ONLY
+    def on_data(self, status):
+        dump(self)
+        decoded = json.loads(status)                                #Decode from json
+        print("====================")                               #Separator          
+        #dump(decoded)                                              #UNCOMMENT THIS TO DUMP DATA
+        if("direct_message" in decoded):                        #======= DATA IS DIRECT MESSAGE
+            directMessageHandler(decoded)
+    def on_error(self, status_code):
+        logText ("Error Code: " + str(status_code))
+        logError(status_code, "Twitter API error. Refer to dev.twitter.com for more informations")
+        if status_code == 420:
+            return False
+        else:
+            return True
+    def on_timeout(self):
+        logError(105, 'Timeout...')
+        return True
+        
+def dump(obj, nested_level=0, output=sys.stdout):
+    """
+        Function used for debugging
+        It writes down in the console every detail of the object sent.
+        params : 
+            obj          : object analysed
+            nested_level : Number of tabs
+            output       : Where to write
+    """
+    spacing = '   '
+    if type(obj) == dict:
+        print ( '%s{' % ((nested_level) * spacing))
+        for k, v in obj.items():
+            if hasattr(v, '__iter__'):
+                print(  '%s%s:' % ((nested_level + 1) * spacing, k))
+                dump(v, nested_level + 1, output)
+            else:
+                print ( '%s%s: %s' % ((nested_level + 1) * spacing, k, v))
+        print (  '%s}' % (nested_level * spacing))
+    elif type(obj) == list:
+        print (  '%s[' % ((nested_level) * spacing))
+        for v in obj:
+            if hasattr(v, '__iter__'):
+                dump(v, nested_level + 1, output)
+            else:
+                print (  '%s%s' % ((nested_level + 1) * spacing, v))
+        print ('%s]' % ((nested_level) * spacing))
+    else:
+        print(  '%s%s' % (nested_level * spacing, obj))
+
 ## The bot itself  
 
 print("BOT " + __program__ + " v" + __version__ + " started. \nConnection...")
@@ -142,51 +368,48 @@ logStart()
 try:
     api.update_profile(location="Sol")
     me = api.me()
-    print("Logged in as @"+ me.screen_name +" !")
+    logText("Logged in as @"+ me.screen_name +" !")
     #print (vars(me))
 except tweepy.TweepError:
-    print ("Could not authenticate you. \nExiting the program.")
+    logText ("Could not authenticate you. \nExiting the program.")
+    logError(420, str(tweepy.TweepError)+" Could not authenticate you.")
     exit()
-print("____[ INFORMATIONS ]____")
-print("@"+me.screen_name)
-print(str(me.followers_count)+" followers : ")
+logText("____[ INFORMATIONS ]____")
+logText("@"+me.screen_name)
+logText(str(me.followers_count)+" followers : ")
 for follower in me.followers():
-   print(" -"+follower.screen_name)
-   follower.follow()
+   logText(" -"+follower.screen_name)
+   if (not follower.following):
+       follower.follow()
 
-myStreamListener = MyStreamListener()
 try:
-    if myStream.running:
-        print ("Stream already running.")
-        myStream.disconnect()
+    if mainStream.running or cmdStream:
+        logText ("Stream already running.")
+        mainStream.disconnect()
+        cmdStream.disconnect()
     else:
-        print("Stream not running. Starting Stream...")
+        logText("Stream not running. Starting Stream...")
 except:
-    print("Stream not Detected. Starting Stream...")
+    logText("Stream not Detected. Starting Stream...")
 
-myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
-myStream.filter(track=['EDPostcards'], async=True)
+mainStream = tweepy.Stream(auth = api.auth, listener=mainStreamListener())
+cmdStream = tweepy.Stream(auth = api.auth, listener=cmdStreamListener())
 
-if myStream.running:
-    print ("Stream running.")
-else:
-    print("Stream startup failed")
+mainStream.filter(track=[hashtag], async=True)
+cmdStream.userstream(async=True)
 shutdown = False
-time.sleep(5)
-while not shutdown:
-    ans = input("Command ? : \n>>>")
-    if (ans == "shutdown"):
-        print("Logging off...")
-        shutdown = True
-        api.update_profile(location="In a debris field")
-        myStream.disconnect()
-        print("Shuting down...")
-        quit()
-    if (ans.startswith("tweet ")):
-        text = ans.strip("tweet ")
-        #print(text)
-        api.update_status(status=text)
-    time.sleep(1)
+
+if mainStream.running:
+    logText ("MainStream running.")
+else:
+    logText("MainStream startup failed")
+    fshutdown()
+if cmdStream.running:
+    logText ("CMDStream running.")
+else:
+    logText("CMDStream startup failed")
+    fshutdown()
+time.sleep(2)
 
 
-
+cmdHandler(input("Command ? : \n>>>"))
